e1fcc278b668bb4dd530eef532d665c9fd70de89
Shankar Vasudevan <shankar@steel-eye.com>
2018-03-09 19:33:50 +0000

    mapresult overload

--- src/main/java/io/polyglotted/common/model/MapResult.java
+++ src/main/java/io/polyglotted/common/model/MapResult.java
@@ -9,13 +9,68 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Delegate;
 
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 import static com.google.common.collect.ImmutableMap.copyOf;
+import static com.google.common.collect.Iterables.getFirst;
+import static io.polyglotted.common.util.Assertions.checkContains;
+import static java.util.Objects.requireNonNull;
 
-@SuppressWarnings({"unused", "deprecation"})
+@SuppressWarnings({"unused", "unchecked", "deprecation", "ConstantConditions"})
 public interface MapResult extends Map<String, Object> {
+
+    default <T> T first() { return requireNonNull(first(null)); }
+
+    @SuppressWarnings("unchecked") default <T> T first(T def) { return (T) getFirst(values(), def); }
+
+    default Entry<String, Object> firstEntry() { return requireNonNull(firstEntry(null)); }
+
+    default Entry<String, Object> firstEntry(Entry<String, Object> def) { return getFirst(entrySet(), def); }
+
+    default String optStr(String prop) { return stringVal(prop, false, null); }
+
+    default String optStr(String prop, String defVal) { return stringVal(prop, false, defVal); }
+
+    default String reqdStr(String prop) { return stringVal(prop, true, null); }
+
+    default boolean boolVal(String prop, boolean defVal) { return asValue(prop, Boolean.class, defVal); }
+
+    default int intVal(String prop, int defVal) { return asValue(prop, Integer.class, defVal); }
+
+    default long longVal(String prop, long defVal) { return asValue(prop, Long.class, defVal); }
+
+    default Long longStrVal(String prop) {
+        Object value = get(prop);
+        return value == null ? null : value instanceof Number ? ((Number) value).longValue() : Long.parseLong(String.valueOf(value));
+    }
+
+    default long longStrVal(String prop, long defaultValue) {
+        Object value = get(prop);
+        return value == null ? defaultValue : value instanceof Number ? ((Number) value).longValue() : Long.parseLong(String.valueOf(value));
+    }
+
+    default String stringVal(String prop, boolean required, String defVal) {
+        return (String) getOrDefault(required ? reqdProp(prop) : prop, defVal);
+    }
+
+    default <T> T optValue(String prop) { return (T) get(prop); }
+
+    default <T> T optValue(String prop, T defValue) { return (T) getOrDefault(prop, defValue); }
+
+    default <T> T reqdValue(String prop) { return (T) get(reqdProp(prop)); }
+
+    default <T> T asValue(String prop, Class<T> clazz, T defVal) { return clazz.cast(getOrDefault(prop, defVal)); }
+
+    default String reqdProp(String prop) { return checkContains(this, prop); }
+
+    default <T> T removeVal(String prop) { return (T) remove(reqdProp(prop)); }
+
+    default <T> T removeIfExists(String prop) { return removeIfExists(prop, null); }
+
+    default <T> T removeIfExists(String prop, T defVl) { return containsKey(prop) ? (T) remove(prop) : defVl; }
 
     static ImmutableMapResult immutableResult() { return new ImmutableMapResult(ImmutableMap.of()); }
 
@@ -81,11 +136,27 @@ public interface MapResult extends Map<String, Object> {
     }
 
     @EqualsAndHashCode @RequiredArgsConstructor class ImmutableMapResult implements ImmutableResult {
-        @Delegate(types = MapResult.class) private final ImmutableMap<String, Object> delegate;
+        @Delegate(types = MapInclude.class) private final ImmutableMap<String, Object> delegate;
 
         @Override public String toString() { return delegate.toString(); }
 
         @Override public ImmutableMapResult immutable() { return this; }
+
+        //@formatter:off
+        private interface MapInclude {
+            int size();
+            boolean isEmpty();
+            boolean containsKey(Object key);
+            boolean containsValue(Object value);
+            Object get(Object key);
+            Object put(String key, Object value);
+            Object remove(Object key);
+            void putAll(Map<? extends String, ?> m);
+            void clear();
+            Set<String> keySet();
+            Collection<Object> values();
+            Set<Entry<String, Object>> entrySet();
+        } //@formatter:on
     }
 
     interface ImmutableResult extends MapResult {
